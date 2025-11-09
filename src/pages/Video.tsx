065e441580c72b4c3a455b0aa226d300096571ac
ZZHow <mail@zzhow.com>
2025-11-09 23:08:30 +0800

    feat: add video introduction page content

--- src/pages/Video.tsx
+++ src/pages/Video.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-
 export default function Video() {
   return (
-    <div>
-      <h2>视频介绍</h2>
-      <p>页面占位：后续将嵌入讲解视频与相关说明。</p>
+    <div style={{ width: '100vw', height: '90vh', margin: 0, padding: 0 }}>
+        <iframe
+            src="//player.bilibili.com/player.html?isOutside=true&aid=114783336534561&bvid=BV14E3tzPEiP&cid=30811357950&p=1"
+            allowFullScreen={true}
+            style={{ width: '100%', height: '100%', border: 'none' }}
+        ></iframe>
     </div>
   )
 }
